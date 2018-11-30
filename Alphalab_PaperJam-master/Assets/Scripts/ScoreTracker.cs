ca19a0c10eb8389c2735d04470adf41361e272d8
unknown <Lavance.Strickland@iam.local>
2018-11-30 15:24:26 -0600

    added ScoreTracker script

---
+++ Alphalab_PaperJam-master/Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ScoreTracker : MonoBehaviour {
+    public static int ScoreMoney = 0;
+    public string textToDisplayBeforeScore;
+    public Text ScoreText;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ScoreText.text = textToDisplayBeforeScore + " $" + ScoreMoney;
+	}
+}
