a62de1811c218f0b1fc8b60b9a0f714d06a0c93c
JSchoppenhorst <44096676+JSchoppenhorst@users.noreply.github.com>
2018-11-30 12:27:21 -0600

    Finished Projectile Shooting

--- Assets/JethroScripts/ProjectileBehaviour.cs
+++ Assets/JethroScripts/ProjectileBehaviour.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 // This goes on each instance of the paper projectile created.
 public class ProjectileBehaviour : MonoBehaviour
 {
+    // A reference to the first projectile object spawned from the prefab.
+    public static GameObject projectilePrefab;
+
     // The rigidbody on this object.
     private Rigidbody2D body;
-
 
     // The speed at which the paper is launched forward.
     [SerializeField]
@@ -18,17 +20,26 @@ public class ProjectileBehaviour : MonoBehaviour
     private float spinSpeed;
 
 
-    // When this projectile is created, spawn it at the players hand.
+    // When a projectile is instantiated:
     private void Start()
     {
-        // Get the rigidbody component on this object.
-        body = transform.GetComponent<Rigidbody2D>();
+        // Is this the first instance of the object?
+        if (FindObjectsOfType<ProjectileBehaviour>().Length == 1)
+        {
+            // If so, set it as the prefab to instantiate from.
+            projectilePrefab = gameObject;
+        }
+        else
+        {
+            // Else, apply the initial velocity values for the projectile.
 
-        // transform.position = Player Position + offset
+            // Get the rigidbody component on this object.
+            body = transform.GetComponent<Rigidbody2D>();
 
-        // Apply the forces for this object to move.
-        body.AddForce(Vector2.up * launchSpeed);
-        body.AddTorque(spinSpeed);
+            // Apply the forces for this object to move.
+            body.AddForce(Vector2.up * launchSpeed);
+            body.AddTorque(spinSpeed);
+        }
     }
 
     // Called when the paper hits a customer.
--- Assets/JethroScripts/ShootFunctionality.cs
+++ Assets/JethroScripts/ShootFunctionality.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Manages the creation of projectiles from user input.
 public class ShootFunctionality : MonoBehaviour {
-	
+
+    // The delay in seconds, between firing shots.
+    [SerializeField]
+    private float shootDelay;
+
+    // Records the last time that a projectile was fired.
+    private float lastShootTime = 0;
+
 	// Update is called once per frame
 	void Update ()
     {
-		if (Input.GetKeyDown("D"))
+        // TODO Replace with GetButtonDown.
+		if (Input.GetKeyDown(KeyCode.D))
         {
-            //GameObject projectile = GameObject.CreatePrimitive()
+            // Has enough time passed to shoot again?
+            if (Time.time - lastShootTime > shootDelay)
+            {
+                // Instantiate a new projectile from the one that is offscreen.
+                // TODO reference the player position when instantiating this object.
+                GameObject newProjectile = Instantiate(ProjectileBehaviour.projectilePrefab, Vector3.zero, Quaternion.identity);
+
+                // Update the last shoot time.
+                lastShootTime = Time.time;
+            }
         }
 	}
 }
