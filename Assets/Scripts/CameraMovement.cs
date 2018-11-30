b7b5fd3099fece80bcc00d5a2e49e4b546dfcc6a
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-11-30 11:52:32 -0600

    Initial Branch for Player Movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField]
+    private Transform playerTransform; //the transform component of the player
+    [SerializeField]
+    private float xOffset; //horizontal camera offset from player position
+    [SerializeField]
+    private float yOffset; //vertical camera offset from player position
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    void setCameraPositionWithOffset()
+    {
+        transform.position = new Vector2(playerTransform.position.x + xOffset, playerTransform.position.y + yOffset);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //fields
+    [SerializeField]
+    private float moveSpeed;
+    
+    //private variables
+    private float horizontalInput;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    void GetInput()
+    {
+        Input.GetAxis("Horizontal");
+    }
+}
