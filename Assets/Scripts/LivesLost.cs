e346bae346f294e195637b5e21dd2fa87240d2e1
Lavance Strickland <lavance_strickland@yahoo.com>
2018-12-01 10:25:43 -0600

    Merge a remastered LivesLost script & menu-ui-transitions

--- Assets/Scripts/LivesLost.cs
+++ Assets/Scripts/LivesLost.cs
@@ -2,21 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LivesStatus { Idle, Reset }
 [RequireComponent(typeof(BoxCollider2D))]
 public class LivesLost : MonoBehaviour {
+    public static LivesStatus state = LivesStatus.Idle;
     [SerializeField]
     private string TagNameForEnemies = "enemyexit";
-    public static int Lives = 3;
+    [SerializeField]
+    private int livesToStartWith = 3;
+    [HideInInspector]
+    public static int Lives;
     [SerializeField]
     private int livesToTake = 1;
-
+    private void Start()
+    {
+        Lives = livesToStartWith;
+    }
+    private void Update()
+    {
+        if (state == LivesStatus.Reset)
+        {
+            Lives = livesToStartWith;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.ToLower() == TagNameForEnemies)
         {
             TakeAwayLife();
         }
-        Debug.Log("Lives: "+Lives);
+        Debug.Log("Lives: " + Lives);
     }
     private void TakeAwayLife()
     {
@@ -25,5 +40,6 @@ public class LivesLost : MonoBehaviour {
         {
             Lives = 0;
         }
+        
     }
 }
